d21994c1b701128a68f9551bee0c51c93e904129
israel dantas <israeldantasleite@gmail.com>
2018-07-03 10:26:26 -0300

    Promises

---
+++ ES6/promises.js
@@ -0,0 +1,26 @@
+/*
+    Into Promise
+*/
+
+const defer = new Promise((resolve, reject) => {
+  let cod = 1
+  setTimeout(() => {
+    if (cod) {
+      resolve('Hello! It works!')
+    } else {
+      reject(new Error('Error'))
+    }
+  }, 2000)
+})
+
+const currency = new Promise((resolve, reject) => {
+  resolve({ currency: 'euro', value: 3.15 })
+})
+
+Promise
+  .all([currency, defer])
+  .then(console.log)
+
+Promise
+  .race([currency, defer])
+  .then(console.log)
